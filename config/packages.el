0444670c69329ead9e4685ab4a748396168bb1b5
Andreas Füglistaler <andreas.fueglistaler@unifr.ch>
2021-10-22 16:02:43 +0200

    removed clang-format as lsp-mode does the same

--- config/packages.el
+++ config/packages.el
@@ -221,16 +221,6 @@
   ("<leader>n" 'flycheck-next-error
    "<leader>p" 'flycheck-previous-error))
 
-;; Format source code
-(use-package clang-format
-  :straight t
-  :hook c-mode-common-hook
-  :general
-  (:states 'motion
-   :keymaps 'c-mode-base-map
-   "=" 'clang-format-region))
-
-
 ;; Dlang
 (use-package d-mode
   :straight t)
